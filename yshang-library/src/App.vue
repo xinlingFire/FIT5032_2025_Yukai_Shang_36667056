c3aff9cd7b778778fe08b191d2b67b649a5fe60c
xinlingFire <ysha0106@student.monash.edu>
2026-07-02 16:06:30 +0800

    Add Assessed Lab 2 library application

---
+++ yshang-library/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import JSONComponent from './components/JSON.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Library Application</h1>
+    <JSONComponent />
+  </main>
+</template>
