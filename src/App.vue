c24affae5413c97cdc0fdf4aa41cb45c5ac66917
xinlingFire <ysha0106@student.monash.edu>
2026-07-12 00:12:21 +0800

    Complete Assessed Lab 3 Bootstrap form and responsive breakpoints

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import JSONComponent from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <main>
+    <!--
     <h1>Library Application</h1>
     <JSONComponent />
+    -->
+
+    <Form />
   </main>
 </template>
