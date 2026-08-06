cc4cb0f1f57782fb069c08404e8f185952ef50b9
xinlingFire <ysha0106@student.monash.edu>
2026-08-07 03:07:23 +0800

    Lab12

--- Assessed Lab 5/vite.config.js
+++ Assessed Lab 5/vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032_2025_Yukai_Shang_36667056/'
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
